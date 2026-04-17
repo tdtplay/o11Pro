c05bf9e2b9f21c4a1868b861e7d18ab03b37ad0d
tdtplay <36959100+tdtplay@users.noreply.github.com>
2026-04-17 19:40:31 +0100

    Update download link for o11Pro.rar

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ cd ffmpeg-7.0.2-amd64-static/
 mv ffmpeg /usr/local/bin/
 mv ffprobe /usr/local/bin/
 cd /root
-wget https://github.com/sibuk76/o11Pro/raw/main/o11Pro.rar
+wget https://github.com/tdtplay/o11Pro/raw/refs/heads/main/o11Pro.rar
 unrar x o11Pro.rar /home
 chmod -R +x /home/o11Pro/
 wget https://raw.githubusercontent.com/sibuk76/o11Pro/main/o11Pro.service
